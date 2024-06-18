f7253e55c6b3b1575587ef25000cbea7cc81cda5
Roman M <prominb@gmail.com>
2024-06-18 05:27:03 +0300

    e007 edit env.py, bd.py and RUN migration

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,8 @@ from sqlalchemy import pool
 from alembic import context
 
 from conf.models import Base
-from conf.db import URI
+# from conf.db import URI
+from conf.db import DATABASE_URL2
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -27,7 +28,8 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-config.set_main_option("sqlalchemy.url", URI)
+# config.set_main_option("sqlalchemy.url", URI)
+config.set_main_option("sqlalchemy.url", DATABASE_URL2)
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
--- conf/db.py
+++ conf/db.py
@@ -1,9 +1,14 @@
-import configparser
+import os
 import pathlib
+import configparser
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
+# loading variables from .env file
+load_dotenv() 
 
 file_config = pathlib.Path(__file__).parent.parent.joinpath("config.ini")
 # print(file_config)
@@ -22,6 +27,9 @@ db = config.get("DEV_DB", "DB_NAME")
 URI = f"postgresql://{user}:{password}@{domain}:{port}/{db}"
 # print(URI)
 
-engine = create_engine(URI, echo=True, pool_size=5, max_overflow=0)
+DATABASE_URL2 = os.getenv('DATABASE_URL2')
+
+engine = create_engine(DATABASE_URL2, echo=True, pool_size=5, max_overflow=0)
+# engine = create_engine(URI, echo=True, pool_size=5, max_overflow=0)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
